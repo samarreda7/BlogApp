2194def28556e8b93387572cb7ff53562e8fe58b
samarreda7 <samarreda804@gmail.com>
2025-08-29 16:56:55 +0300

    Initial commit

---
+++ BlogApp.EF/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using BlogApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.EF
+{
+    public class UnitOfWork : IUnitOfWork
+
+    {
+    }
+}
---
+++ BlogApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlogApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
